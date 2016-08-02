5a583ff4fbecdb436fa2ea76f152c65b6e183da2
Ronak Panahi <RonakPanahi@users.noreply.github.com>
2016-08-02 11:32:15 -0500

    Add files via upload

--- client_side/Client.java
+++ client_side/Client.java
@@ -1,1 +1,35 @@
+package client_side;
 
+
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+
+import server_side.CurrentTime;
+
+
+@SuppressWarnings("deprecation")
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		try {
+			System.setProperty("java.security.policy","file:E:\\UIS\\Eclipse\\RMI_Project\\pp.policy");
+			System.setSecurityManager(new RMISecurityManager());
+			
+			
+			CurrentTime obj = (CurrentTime) Naming.lookup( "rmi://localhost" + 
+			"/GetTime");//object name in registry
+			
+			System.out.println(obj.getTime()); 
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			 System.err.println("Client exception: " + e.getMessage()); 
+			 e.printStackTrace(); 
+		}
+	}
+
+
+}
